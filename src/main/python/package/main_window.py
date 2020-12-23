848962ccac80789b85b2101ff27d4a0268b290cc
RiviereGregory <gregory.riviere@laposte.net>
2020-12-23 14:46:08 +0100

    Ajout creation Layout et ajout feuille de style

--- src/main/python/package/main_window.py
+++ src/main/python/package/main_window.py
@@ -1,15 +1,16 @@
-from PySide2 import QtWidgets
+from PySide2 import QtWidgets, QtCore
+
 
 class MainWindow(QtWidgets.QMainWindow):
-    def __init__(self):
+    def __init__(self, ctx):
         super().__init__()
+        self.ctx = ctx
         self.setup_ui()
-
 
     def setup_ui(self):
         self.create_widgets()
-        self.modify_widgets()
         self.create_layouts()
+        self.modify_widgets()
         self.add_widgets_to_layouts()
         self.setup_connections()
 
@@ -20,15 +21,20 @@ class MainWindow(QtWidgets.QMainWindow):
         self.sld_iconSize = QtWidgets.QSlider()
         self.main_widget = QtWidgets.QWidget()
 
-
     def modify_widgets(self):
-        pass
+        css_file = self.ctx.get_resource("style.css")
+        with open(css_file, "r") as f:
+            self.setStyleSheet(f.read())
 
     def create_layouts(self):
-        pass
+        self.main_layout = QtWidgets.QHBoxLayout(self.main_widget)
 
     def add_widgets_to_layouts(self):
-        pass
+        self.addToolBar(QtCore.Qt.TopToolBarArea, self.toolbar)
+        self.setCentralWidget(self.main_widget)
+        self.main_layout.addWidget(self.tree_view)
+        self.main_layout.addWidget(self.list_view)
+        self.main_layout.addWidget(self.sld_iconSize)
 
     def setup_connections(self):
         pass
